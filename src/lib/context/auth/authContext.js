433bed6cf7ac81a3e9c38eca76d8a62799f364fc
Ander <ibysytho@gmail.com>
2021-04-06 17:41:50 +0200

    reformatresolution hook & auth context + book section menu

--- src/lib/context/auth/authContext.js
+++ src/lib/context/auth/authContext.js
@@ -2,7 +2,6 @@
 import { useReducer, createContext } from 'react';
 import AuthReducer from './authReducer';
 import { SET_USER, SET_UI_STATE, SET_IS_LOADING } from 'enums/types';
-
 const AuthContext = createContext(null);
 
 export const AuthState = ({ children }) => {
@@ -14,36 +13,17 @@ export const AuthState = ({ children }) => {
 
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
-  const setUser = (user) => {
-    dispatch({
-      type: SET_USER,
-      payload: user,
-    });
-  };
-
-  const setUiState = (uiState) => {
-    dispatch({
-      type: SET_UI_STATE,
-      payload: uiState,
-    });
-  };
-
-  const setIsLoading = (isLoading) => {
-    dispatch({
-      type: SET_IS_LOADING,
-      payload: isLoading,
-    });
-  };
-
   return (
     <AuthContext.Provider
       value={{
         user: state.user,
         uiState: state.uiState,
         isLoading: state.isLoading,
-        setUser,
-        setUiState,
-        setIsLoading,
+        setUser: (user) => dispatch({ type: SET_USER, payload: user }),
+        setUiState: (uiState) =>
+          dispatch({ type: SET_UI_STATE, payload: uiState }),
+        setIsLoading: (isLoading) =>
+          dispatch({ type: SET_IS_LOADING, payload: isLoading }),
       }}
     >
       {children}
--- src/lib/context/auth/authReducer.js
+++ src/lib/context/auth/authReducer.js
@@ -1,25 +1,23 @@
 import { SET_USER, SET_UI_STATE, SET_IS_LOADING } from 'enums/types';
 
+const ACTIONS_REDUCERS = {
+  [SET_USER]: (state, action) => ({
+    ...state,
+    user: action.payload,
+  }),
+  [SET_UI_STATE]: (state, action) => ({
+    ...state,
+    uiState: action.payload,
+  }),
+  [SET_IS_LOADING]: (state, action) => ({
+    ...state,
+    isLoading: action.payload,
+  }),
+};
+
 const AuthReducer = (state, action) => {
-  switch (action.type) {
-    case SET_USER:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case SET_UI_STATE:
-      return {
-        ...state,
-        uiState: action.payload,
-      };
-    case SET_IS_LOADING:
-      return {
-        ...state,
-        isLoading: action.payload,
-      };
-    default:
-      return state;
-  }
+  const actionReducer = ACTIONS_REDUCERS[action.type];
+  return actionReducer ? actionReducer(state, action) : state;
 };
 
 export default AuthReducer;
